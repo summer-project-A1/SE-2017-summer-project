acea9df8a420372e6a047d212ea395d72db89aad
huangzheng <huangzheng@sjtu.edu.cn>
2017-06-30 10:15:10 +0800

    购物车功能（未测试）

--- src/main/java/dao/impl/BookDaoImpl.java
+++ src/main/java/dao/impl/BookDaoImpl.java
@@ -1,9 +1,20 @@
 package dao.impl;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.bson.types.ObjectId;
 import org.hibernate.query.Query;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSDBFile;
+import com.mongodb.gridfs.GridFSInputFile;
 
 import dao.BaseDao;
 import dao.BookDao;
@@ -12,6 +23,15 @@ import model.User;
 
 public class BookDaoImpl extends BaseDaoImpl implements BookDao {
 
+    @Override
+    public Book getBookByID(int bookID) {
+        String hql = "from Book b where b.bookID = :bookID";
+        Query query = getSession().createQuery(hql).setParameter("bookID", bookID);
+        List<Book> books = query.list();
+        Book book = books.size() == 1 ? books.get(0) : null;
+        return book;
+    }
+    
     @Override
     public List<Book> getAllBooks() {
         String hql = "from Book";
@@ -37,8 +57,27 @@ public class BookDaoImpl extends BaseDaoImpl implements BookDao {
 
     @Override
     public Map getBookProfileMap(int bookID) {
-        // TODO 自动生成的方法存根
-        return null;
+        DBCollection collection = getMongoDb().getCollection("book_profile");
+        DBObject query=new BasicDBObject("book_id", bookID);
+        DBObject obj = collection.findOne(query);
+        Map bookProfile = (obj!=null) ? (Map)obj : null;
+        return bookProfile;
+    }
+
+    @Override
+    public boolean addOrUpdateBookProfile(Map bookProfile) {
+        DBCollection collection = getMongoDb().getCollection("book_profile");
+        DBObject query=new BasicDBObject("book_id", (int)bookProfile.get("bookID"));
+        DBObject old = collection.findOne(query);
+
+        BasicDBObject document = new BasicDBObject(bookProfile);
+        if(old!=null) {
+            collection.update(query, document);
+        }
+        else {
+            collection.insert(document);
+        }
+        return true;
     }
     
 }
