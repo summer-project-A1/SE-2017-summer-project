15fc4dd86150f24f4f9465ccfc1cb391e69cf0b6
huangzheng <huangzheng@sjtu.edu.cn>
2017-07-10 16:26:25 +0800

    a

--- src/main/java/action/BorrowAction.java
+++ src/main/java/action/BorrowAction.java
@@ -3,5 +3,9 @@ package action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class BorrowAction extends ActionSupport {
-    
+
+    public String createBorrowOrder() {
+        //this.params = this.borrowService.borrowAllBookInBorrowCart();
+        return "borrow";
+    }
 }
