c5afa705ea09ac75cddd932bea52bd22b2e5f92c
huangzheng <huangzheng@sjtu.edu.cn>
2017-07-14 19:15:33 +0800

    a

--- src/main/java/action/IndexAction.java
+++ src/main/java/action/IndexAction.java
@@ -1,7 +1,12 @@
 package action;
 
+import java.util.List;
+
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+
+import model.Category1;
+import model.Category2;
 import service.BookService;
 
 /**
@@ -16,10 +21,18 @@ public class IndexAction extends ActionSupport {
         this.bookService = bookService;
     }
     public String index() {
-        ActionContext.getContext().put("title","Í¼Êé·ÖÏí½»Á÷Æ½Ì¨");
+        ActionContext.getContext().put("title","Í¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ½Ì¨");
         return "showIndex";
     }
     public String header(){
+        List<Category1> c1 = this.bookService.showAllCategory1s();
+        for(Category1 t1 : c1) {
+            System.out.println(t1.getCategory1Name());
+            List<Category2> c2 = t1.getCategory2List();
+            for(Category2 t2 : c2) {
+                System.out.println(t2.getCategory2Name());
+            }
+        }
         ActionContext.getContext().put("category1List",bookService.showAllCategory1s());
         return"showHeader";
     }
--- src/main/webapp/header.jsp
+++ src/main/webapp/header.jsp
@@ -1,6 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="s" uri="/struts-tags" %>
-<%@include file="global.jsp"%>
+<%//@include file="global.jsp"%>
+<%
+    String path = request.getContextPath();
+    String basePath = request.getScheme() + "://"
+            + request.getServerName() + ":" + request.getServerPort()
+            + path + "/";
+%>
 <!DOCTYPE html>
 
 <html>
