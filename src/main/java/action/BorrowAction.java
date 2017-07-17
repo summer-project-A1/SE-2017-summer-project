2aba256a3429051cf47db4c117a8d22ebcac9d1c
huangzheng <huangzheng@sjtu.edu.cn>
2017-07-17 14:54:54 +0800

    createBorrowOrder

--- src/main/java/action/BorrowAction.java
+++ src/main/java/action/BorrowAction.java
@@ -23,6 +23,8 @@ public class BorrowAction extends ActionSupport {
     private int bookID;
     private Map params;
 
+    private String address;
+    
     /* ============================================================== */
     
     public BorrowService getBorrowService() {
@@ -58,6 +60,12 @@ public class BorrowAction extends ActionSupport {
 
     /* ============================================================== */
     
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
     public String borrowCheckout() {        // 从购物车跳转到地址确认页面，不修改数据库
         Map borrowInfo = this.cartService.showBorrowCart();
         List<Book> booksInBorrowCart = (List<Book>)borrowInfo.get("booksInBorrowCart");
@@ -71,13 +79,13 @@ public class BorrowAction extends ActionSupport {
         return "borrowCheckout";
     }
     
-    public String createBorrowOrder() {     // 用户创建订单，添加到数据库，跳转到付款页面
-        /*this.borrowService.borrowAllBookInBorrowCart();
-         */
-        //List<BorrowProfile> borrowProfileList = this.borrowService.<service的函数名>(this.address);
+    public String createBorrowOrder() {     // 用户创建订单，添加到数据库，跳转到付款页面（不修改书的状态）
+        Map result = this.borrowService.borrowAllBookInBorrowCart(this.address);
+        List<BorrowProfile> borrowProfileList = (List<BorrowProfile>)result.get("borrowProfileList");
+        Integer totalCredit = (Integer)result.get("totalCredit");
         ActionContext.getContext().put("buyOrBorrow","borrow");
-        //ActionContext.getContext().put("totalCredit",<从service层传来>）;
-        //ActionContext.getContext().put("borrowProfileList",borrowProfileList);
+        ActionContext.getContext().put("totalCredit",totalCredit);
+        ActionContext.getContext().put("borrowProfileList",borrowProfileList);
         return "createBorrowOrder";
     }
     public String confirmBorrowOrder() {       // 用户付款确认订单（允许多个订单），修改订单状态
