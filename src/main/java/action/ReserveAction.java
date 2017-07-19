e293173c2ec99b22779651f1065404a923de1e7d
huangzheng <huangzheng@sjtu.edu.cn>
2017-07-19 15:40:26 +0800

    预约

--- src/main/java/action/ReserveAction.java
+++ src/main/java/action/ReserveAction.java
@@ -2,6 +2,7 @@ package action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import service.ReserveService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,14 +11,32 @@ import java.util.Map;
  * Created by zzy on 2017/7/5.
  */
 public class ReserveAction extends ActionSupport{
-
-
+    
+    private ReserveService reserveService;
+    
     private Map params;
 
-   private int bookID;
+    private int bookID;
 
 
     /* =========================================================== */
+    
+    public ReserveService getReserveService() {
+        return reserveService;
+    }
+
+    public void setReserveService(ReserveService reserveService) {
+        this.reserveService = reserveService;
+    }
+
+    public Map getParams() {
+        return params;
+    }
+
+    public void setParams(Map params) {
+        this.params = params;
+    }
+
     public int getBookID() {
         return bookID;
     }
@@ -31,7 +50,7 @@ public class ReserveAction extends ActionSupport{
     public String reserveBook() {
         params = new HashMap();
         //return value: "success","already","error"
-        //params.put("result",this.reserveService.reserveBook(bookID));
+        params.put("result",this.reserveService.reserveBook(bookID));
         params.put("success","success");
 
         return "ajax";
@@ -41,7 +60,7 @@ public class ReserveAction extends ActionSupport{
         this.params = new HashMap();
         params = new HashMap();
         //return value: true, false
-        //params.put("result",this.reserveService.cancelReservation(bookID));
+        params.put("result",this.reserveService.cancelReservation(bookID));
         params.put("success", true);
 
         return "ajax";
--- src/main/java/service/ReserveService.java
+++ src/main/java/service/ReserveService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import model.Reserve;
 
 public interface ReserveService extends BaseService {
-    public Boolean reserveBook(int bookID);
+    public String reserveBook(int bookID);
     public List<Reserve> showReservation();
     public Boolean cancelReservation(int applyID);
 }
