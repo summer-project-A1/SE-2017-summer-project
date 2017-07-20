c2cdd6291cf2e874c82eb59becdab7d0f927d17a
ljw9609 <473100319@qq.com>
2017-07-20 16:32:51 +0800

    我的预约功能

--- src/main/java/service/ReserveService.java
+++ src/main/java/service/ReserveService.java
@@ -2,10 +2,11 @@ package service;
 
 import java.util.List;
 
+import model.ReservationProfile;
 import model.Reserve;
 
 public interface ReserveService extends BaseService {
     public String reserveBook(int bookID);
-    public List<Reserve> showReservation();
-    public Boolean cancelReservation(int applyID);
+    public List<ReservationProfile> showReservation();
+    public Boolean cancelReservation(int reserveID);
 }
