c2058e0c38d6f4898675d43d101eb0543e744768
huangzheng <huangzheng@sjtu.edu.cn>
2017-07-20 13:57:15 +0800

    exchange dao

--- src/main/java/dao/ExchangeDao.java
+++ src/main/java/dao/ExchangeDao.java
@@ -6,5 +6,6 @@ import model.Exchange;
 
 public interface ExchangeDao extends BaseDao {
 	public Exchange getExchangeByID(int id);
-    public List<Exchange> getExchangeByUserID(int userID);
+    public List<Exchange> getExchangeByUserID1(int userID1);
+    public List<Exchange> getExchangeByUserID2(int userID2);
 }
